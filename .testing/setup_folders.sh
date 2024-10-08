8cd4cb3bdbe6045408ee190df16a3106cc66cba1
MarkusSprunck <sprunck.markus@gmail.com>
2024-10-08 05:39:06 +0000

    CBC-893 Cleanup Leopold Testing

--- .testing/setup_folders.sh
+++ .testing/setup_folders.sh
@@ -1,17 +1,19 @@
 
+sudo rm -rf /home/parity
+
 sudo mkdir -p /home/parity 
 sudo chown -R codespace:codespace /home/parity 
 
 mkdir -p /home/parity/chain
 mkdir -p /home/parity/data
 mkdir -p /home/parity/data/network
-mkdir -p /home/parity/data/keys/
+mkdir -p /home/parity/data/keys
 
 ln -sf  ${PWD}/dist/staging/authority.toml           /home/parity/authority.toml
 ln -sf  ${PWD}/dist/staging/reserved_peers           /home/parity/chain/reserved_peers
 ln -sf  ${PWD}/dist/staging/spec.json                /home/parity/chain/spec.json
 ln -sf  ${PWD}/dist/staging/password                 /home/parity/password
-ln -sf  ${PWD}/dist/staging/leopold                  /home/parity/data/keys/leopold
+ln -sf  ${PWD}/dist/staging/leopold                  /home/parity/data/keys
 ln -sf  ${PWD}/dist/staging/key.priv                 /home/parity/data/network/key
 
 tree /home/parity/
