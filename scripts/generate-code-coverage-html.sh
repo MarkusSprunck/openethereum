7645478a9952161a7bdd35a395d2673ac17e6f76
Michael S. <michael.schott@netlight.com>
2024-10-23 15:30:17 +0000

    CBC-919 (Code Coverage): add coments generate code coverage scripts (tarpaulin)

--- scripts/generate-code-coverage-html.sh
+++ scripts/generate-code-coverage-html.sh
@@ -3,6 +3,10 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
+# Running those locally takes really long.
+# Some tests `timeout` with Tarpaulin, although they do not when run with `cargo`.
+# For this reason, use the --no-fail-fast flag.
+
 cargo install cargo-tarpaulin
 git submodule init
 git submodule update
