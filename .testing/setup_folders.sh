51ed3dce1a195029180241e86e008521c8b6f43d
Markus Sprunck <sprunck.markus@gmail.com>
2025-06-27 12:47:04 +0000

    CBC-971 Prepare Development Environment for OpenEthereum 3.5.0 (Visual Studio Code)
    - cleanup cargo.toml
    - don't delete test blockchain by default

--- .testing/setup_folders.sh
+++ .testing/setup_folders.sh
@@ -1,19 +1,19 @@
-echo "Recreate Configuration and Delete Blockchain Data"
-sudo rm -rf /home/parity
+# echo "Recreate Configuration and Delete Blockchain Data"
+# sudo rm -rf /home/parity
 
-sudo mkdir -p /home/parity
-sudo chown -R ${USER}:${GROUP} /home/parity
+# sudo mkdir -p /home/parity
+# sudo chown -R ${USER}:${GROUP} /home/parity
 
-mkdir -p /home/parity/chain
-mkdir -p /home/parity/data
-mkdir -p /home/parity/data/network
-mkdir -p /home/parity/data/keys
+# mkdir -p /home/parity/chain
+# mkdir -p /home/parity/data
+# mkdir -p /home/parity/data/network
+# mkdir -p /home/parity/data/keys
 
-ln -sf  ${PWD}/dist/staging/authority.toml           /home/parity/authority.toml
-ln -sf  ${PWD}/dist/staging/reserved_peers           /home/parity/chain/reserved_peers
-ln -sf  ${PWD}/dist/staging/spec.json                /home/parity/chain/spec.json
-ln -sf  ${PWD}/dist/staging/password                 /home/parity/password
-ln -sf  ${PWD}/dist/staging/leopold                  /home/parity/data/keys
-ln -sf  ${PWD}/dist/staging/key.priv                 /home/parity/data/network/key
+# ln -sf  ${PWD}/dist/staging/authority.toml           /home/parity/authority.toml
+# ln -sf  ${PWD}/dist/staging/reserved_peers           /home/parity/chain/reserved_peers
+# ln -sf  ${PWD}/dist/staging/spec.json                /home/parity/chain/spec.json
+# ln -sf  ${PWD}/dist/staging/password                 /home/parity/password
+# ln -sf  ${PWD}/dist/staging/leopold                  /home/parity/data/keys
+# ln -sf  ${PWD}/dist/staging/key.priv                 /home/parity/data/network/key
 
 tree -hup /home/parity/
