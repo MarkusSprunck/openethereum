134bc6f1a4cbc5a52c72ab420023de0f800bb4e6
Markus Sprunck <sprunck@muenchen.ihk.de>
2025-07-05 13:14:15 +0200

    CBC-930 patch generic-array to 0.12.4 (rollback)
    - rollback to commit #7f9930b

--- .testing/test-leopold.sh
+++ .testing/test-leopold.sh
@@ -4,6 +4,6 @@ set -e # fail on any error
 set -u # treat unset variables as error
 
 cd ..
-# export CC=$(which gcc-12) &&\
-# export CXX=$(which g++-12) &&\
+export CC=$(which gcc-12) &&\
+export CXX=$(which g++-12) &&\
 cargo run --color=always --release --features final -- --config $(pwd)/.testing/dist/authority.toml
