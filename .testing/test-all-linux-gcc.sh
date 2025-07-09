300ac6eebe23b44356da63656c113b9c8a21f5a5
Markus Sprunck <sprunck@muenchen.ihk.de>
2025-07-09 08:56:25 +0200

    CBC-983 Cleanup
    - remove target x86_64-apple-darwin (no further activities planned)

--- .testing/test-all-linux-gcc.sh
+++ .testing/test-all-linux-gcc.sh
@@ -3,8 +3,7 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-cd ..
 echo "_____ Switch to CGG _____"
-export CC=$(which gcc-12) &&\
-export CXX=$(which g++-12) &&\
+cd ..
+export CC=$(which gcc-12) && export CXX=$(which g++-12) &&\
 time cargo test --all
--- .testing/test-all-macos-arm64.sh
+++ .testing/test-all-macos-arm64.sh
@@ -4,6 +4,6 @@ set -e # fail on any error
 set -u # treat unset variables as error
 
 echo "_____ Switch to Clang _____"
-export CC=/usr/bin/clang &&\
-export CXX=/usr/bin/clang++ &&\
-cargo test --all
+cd ..
+export CC=/usr/bin/clang && export CXX=/usr/bin/clang++ &&\
+time cargo test --all
